9366e5effa9c261d6704cff1b38ddcbd9128163d
Vanduytran01 <90382738+Vanduytran01@users.noreply.github.com>
2025-02-22 22:59:03 +0700

    Update api_xoayproxy.py

--- api_xoayproxy.py
+++ api_xoayproxy.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+import ssl
 import os
 class SimpleHandler(BaseHTTPRequestHandler):
     def do_GET(self):
@@ -10,14 +11,14 @@ class SimpleHandler(BaseHTTPRequestHandler):
             global index, danhsach, port, chaylai
             if index < len(danhsach):
                 self.wfile.write("Request received on /changeip\n")
-                os.system('bash upstream.sh "{}" "{}" "{}" &'.format(":".join(danhsach[index].split(":")[:2]),":".join(danhsach[index].split(":")[2:]), port))
+                os.system('bash upstream.sh "{}" "{}" "{}"'.format(":".join(danhsach[index].split(":")[:2]),":".join(danhsach[index].split(":")[2:]), port))
                 index += 1
             elif not chaylai:
                 self.wfile.write("Cannot change (out of proxy)\n")
             else:
                 self.wfile.write("Request received on /changeip\n")
                 index = 0
-                os.system('bash upstream.sh "{}" "{}" "{}" &'.format(":".join(danhsach[index].split(":")[:2]),":".join(danhsach[index].split(":")[2:]), port))
+                os.system('bash upstream.sh "{}" "{}" "{}"'.format(":".join(danhsach[index].split(":")[:2]),":".join(danhsach[index].split(":")[2:]), port))
                 index += 1
         else:
             self.send_response(404)
@@ -41,10 +42,14 @@ if __name__ == "__main__":
         chaylai = True
     else:
         chaylai = False
-    os.system('bash upstream.sh "{}" "{}" "{}" &'.format(":".join(danhsach[index].split(":")[:2]),":".join(danhsach[index].split(":")[2:]), port))
+    os.system('bash upstream.sh "{}" "{}" "{}"'.format(":".join(danhsach[index].split(":")[:2]),":".join(danhsach[index].split(":")[2:]), port))
     index += 1
     server_address = ('0.0.0.0', 1234)
     httpd = HTTPServer(server_address, SimpleHandler)
+    httpd.socket = ssl.wrap_socket(httpd.socket,
+                                   keyfile="server.key",    # Đường dẫn đến khóa riêng
+                                   certfile="server.crt",   # Đường dẫn đến chứng chỉ SSL
+                                   server_side=True)
 
     print("Server running at http://127.0.0.1:1234...")
     print("LINK CHANGE IP: http://IP:1234/changeip")
